5329f0845e78743d02a428f4e0216b5b0ee91bde
Oliver Joel Locon Lopez <106788709+OliJoel0411@users.noreply.github.com>
2022-10-26 11:02:21 -0600

    Update Main.java
    Tipos de datos primitivos

--- FundamentosDeProgramacion/src/Main.java
+++ FundamentosDeProgramacion/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         // Tipos de datos
+        byte num = 100;
+        short num2 = 3200;
+        int num3 = 2000000;
+        long num4 = 1213516516;
+        float num5 = 1.2696516f; // se escribe una "F" al final para que identifíque que es float.
+        double num6 = 1.956161919181;
+        char caracter = 'a';
+        boolean var = true; // o false
+
+        System.out.println("byte: "+ num);
+        System.out.println("short: "+ num2);
+        System.out.println("int: "+ num3);
+        System.out.println("long: "+ num4);
+        System.out.println("float: "+ num5);
+        System.out.println("double: "+ num6);
+        System.out.println("char: "+ caracter);
+        System.out.println("boolean: "+ var);
     }
 }

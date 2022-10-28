acac4aebc057b0adf2619dd1d7c3ab64c8969447
Olijoel0411 <oliverloconlopez@gmail.com>
2022-10-28 09:39:47 -0600

    Ejercicio 2

--- Fundamentos_De_Programacion/src/EjerciciosPropuestosEstructurasDeProgramacion/Ejercicio2.java
+++ Fundamentos_De_Programacion/src/EjerciciosPropuestosEstructurasDeProgramacion/Ejercicio2.java
@@ -1,11 +1,33 @@
 package EjerciciosPropuestosEstructurasDeProgramacion;
 
+import java.util.Scanner;
+
 /**
  *
  * @author oliver
  */
 public class Ejercicio2 {
     public static void main(String[] args) {
+        Scanner tc = new Scanner(System.in);
         
+        //Variables 
+        
+        int n;
+        int factorial = 1;
+       
+        
+        System.out.println("Calculemos el factorial de un numero");
+        System.out.println("Ingrese un numero");
+        n = tc.nextInt();
+        if(n == 0){
+            System.out.println("El factorial de: " + n + " es: 1");
+        }else if(n == 1 || n == 2){
+            System.out.println("El factorial de: " + n + " es: " + n);
+        }else {
+            for (int i = n; i > 0; i--) {
+                factorial = factorial*i;
+            }
+        }
+        System.out.println(factorial);
     }
 }

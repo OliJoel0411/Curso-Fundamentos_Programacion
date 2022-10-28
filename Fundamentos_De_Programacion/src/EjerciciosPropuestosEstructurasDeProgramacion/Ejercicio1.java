26c30917b77c7c163eb67d90c578ace7cf6a21c2
Olijoel0411 <oliverloconlopez@gmail.com>
2022-10-28 08:42:49 -0600

    Ejercicio2

--- Fundamentos_De_Programacion/src/EjerciciosPropuestosEstructurasDeProgramacion/Ejercicio1.java
+++ Fundamentos_De_Programacion/src/EjerciciosPropuestosEstructurasDeProgramacion/Ejercicio1.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package EjerciciosPropuestosEstructurasDeProgramacion;
 
 import java.util.Scanner;
 
 /**
  *
- * @author olive
+ * @author oliver
  */
 public class Ejercicio1 {
     public static void main(String[] args) {
--- Fundamentos_De_Programacion/src/EjerciciosPropuestosEstructurasDeProgramacion/Ejercicio2.java
+++ Fundamentos_De_Programacion/src/EjerciciosPropuestosEstructurasDeProgramacion/Ejercicio2.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package EjerciciosPropuestosEstructurasDeProgramacion;
 
 /**
  *
- * @author olive
+ * @author oliver
  */
 public class Ejercicio2 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
